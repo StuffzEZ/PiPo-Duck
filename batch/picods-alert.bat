a78280b76df86bcab46401c4199fc833d2a0d54a
StuffzEZ (Aka Stuf_y) <158745985+StuffzEZ@users.noreply.github.com>
2025-07-30 10:04:27 +0100

    Update picods-alert.bat

--- batch/picods-alert.bat
+++ batch/picods-alert.bat
@@ -1,17 +1,19 @@
 @echo off
 :: Create temporary VBScript for popup
 echo Set objShell = CreateObject("WScript.Shell") > %temp%\popup.vbs
-echo objShell.Popup "This means someone has plugged a PiPo ducky into your computer and ran this script to warn you! They are kind for doing that since its a hacking tool :3", 5, "You should be grateful!", 64 >> %temp%\popup.vbs
+echo objShell.Popup "This message is shown because someone has plugged a PiPo ducky into your computer and ran this script to warn you! They are kind for doing that since its a hacking tool :3", 0, "You should be grateful!", 64 >> %temp%\popup.vbs
+cls
 
-:: Show the popup
+:: Show the popup (waits for user to close it)
 cscript //nologo %temp%\popup.vbs
 del %temp%\popup.vbs
+cls
 
-:: Create a second batch file to delete this one
+:: Create a second batch file to delete this one AFTER popup closes
 > %temp%\delself.bat echo @echo off
->> %temp%\delself.bat echo ping 127.0.0.1 -n 2 >nul
+>> %temp%\delself.bat echo ping 127.0.0.1 -n 4 >nul
 >> %temp%\delself.bat echo del "%~f0"
 
-:: Run the delete script and exit
+:: Run the delete script in background
 start "" /b %temp%\delself.bat
 exit
