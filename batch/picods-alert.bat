f1db3b8d18d1b63e2ea8dd504af3475c0ab0b3a5
StuffzEZ (Aka Stuf_y) <158745985+StuffzEZ@users.noreply.github.com>
2025-07-30 10:12:50 +0100

    Update picods-alert.bat

--- batch/picods-alert.bat
+++ batch/picods-alert.bat
@@ -1,17 +1,18 @@
 @echo off
-:: -------------------------------
-:: Minimize the current CMD window
-:: -------------------------------
-echo Set objShell = CreateObject("WScript.Shell") > %temp%\min.vbs
-echo objShell.SendKeys "%% n" >> %temp%\min.vbs
-cscript //nologo %temp%\min.vbs >nul
-del %temp%\min.vbs
+:: Enable ANSI escape sequences (Windows 10+)
+:: Clear the screen and print red text
+
+:: Use prompt trick to get ESC char into variable
+for /F "delims=" %%A in ('echo prompt $E ^| cmd') do set "ESC=%%A"
+
+cls
+echo %ESC%[31mRead the Pop Up%ESC%[0m
 
 :: -------------------------------
 :: Create temporary VBScript for popup
 :: -------------------------------
 echo Set objShell = CreateObject("WScript.Shell") > %temp%\popup.vbs
-echo objShell.Popup "This message is shown because someone has plugged a PiPo ducky into your computer and ran this script to warn you! They are kind for doing that since it's a hacking tool :3", 0, "You should be grateful!", 64 >> %temp%\popup.vbs
+echo objShell.Popup "This means someone has plugged a PiPo ducky into your computer and ran this script to warn you! They are kind for doing that since it's a hacking tool :3", 0, "You should be grateful!", 64 >> %temp%\popup.vbs
 
 :: Show the popup and wait until user closes it
 cscript //nologo %temp%\popup.vbs
