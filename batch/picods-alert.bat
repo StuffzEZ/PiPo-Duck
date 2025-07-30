568838f70f9ce16e581b7862f1c2f85beb7c8223
StuffzEZ (Aka Stuf_y) <158745985+StuffzEZ@users.noreply.github.com>
2025-07-30 09:57:04 +0100

    Update picods-alert.bat

--- batch/picods-alert.bat
+++ batch/picods-alert.bat
@@ -1,7 +1,7 @@
 @echo off
 :: Create temporary VBScript for popup
 echo Set objShell = CreateObject("WScript.Shell") > %temp%\popup.vbs
-echo objShell.Popup "This is your alert message!", 5, "Alert Title", 64 >> %temp%\popup.vbs
+echo objShell.Popup "This means someone has plugged a PiPo ducky into your computer and ran this script to warn you! They are kind for doing that since its a hacking tool :3", 5, "You should be grateful!", 64 >> %temp%\popup.vbs
 
 :: Show the popup
 cscript //nologo %temp%\popup.vbs
